52a27a83be526ad88d9a6423e3859b09fb8e0951
Juan Esteban Brand Tovar <jose.becerra@correounivalle.edu.co - brand.juan@correounivalle.edu.co - juan.pablo.pantoja@correounivalle.edu.co>
2023-05-25 21:57:18 -0500

    Cambio de estructura del servidor

---
+++ server/src/app.js
@@ -0,0 +1,30 @@
+import expres from "express"
+import mongoose from "mongoose"
+import morgan from "morgan"
+import router from "../src/routes/proyecto.js"
+import cors from "cors"
+
+//App root
+const app = expres();
+
+//Connection database
+mongoose.connect('mongodb+srv://brandjuan:nDV1dZYmQCH7bbq4@civilizacion-china.p8tyooj.mongodb.net/CivilizacionesDB')
+    .then(()=>{
+        console.log('Conectado a la base de datos')
+    })
+
+//Config
+app.set('PORT', 5000)
+
+//Middleware
+app.use(morgan('dev'))
+app.use(expres.json())
+app.use(cors({origin: ['http://localhost:3000']}))
+
+//Routes
+app.use('/api/', router)
+
+//On server
+app.listen(app.get('PORT'), ()=>{
+    console.log('Server port ' + app.get('PORT'))
+})
---
+++ server/src/routes/proyecto.js
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import User from "../models/proyecto.js"
+
+
+//Routes
+const router = Router();
+
+//Solicitudes de tipo GET
+router.get('/users', async(req,res)=>{
+    const users = await User.find();
+    res.send(users)
+})
+
+export default router;
