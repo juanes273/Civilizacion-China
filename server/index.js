a5f747da01e1cf7adb39677538f750762ca386bd
Juan Esteban Brand Tovar <jose.becerra@correounivalle.edu.co - brand.juan@correounivalle.edu.co - juan.pablo.pantoja@correounivalle.edu.co>
2023-05-26 14:19:19 -0500

    vercel

--- server/index.js
+++ server/index.js
@@ -5,7 +5,6 @@ import http from "http";
 import mongoose from "mongoose";
 import "dotenv/config";
 import routes from "./src/routes/proyecto.js";
-import User from "../models/proyecto.js"
 
 const app = express();
 
@@ -18,7 +17,7 @@ app.use(cookieParser());
 
 app.use("/api/", routes);
 
-const port = process.env.PORT || 5000;
+//const port = process.env.PORT || 5000;
 
 const server = http.createServer(app);
 
