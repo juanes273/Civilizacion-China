24a9ab42c8adaeb92e944d480c8cbb548c6ba183
Juan Esteban Brand Tovar <jose.becerra@correounivalle.edu.co - brand.juan@correounivalle.edu.co - juan.pablo.pantoja@correounivalle.edu.co>
2023-05-26 13:33:44 -0500

    cambios en el servidor

--- server/index.js
+++ server/index.js
@@ -8,6 +8,8 @@ import routes from "./src/routes/proyecto.js";
 
 const app = express();
 
+mongoose.set('strictQuery', false);
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
