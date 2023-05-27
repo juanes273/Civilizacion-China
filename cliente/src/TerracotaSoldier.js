414ad86cd71d9b53416c2d283842967072884bbb
Neón <juan.pablo.pantoja@correounivalle.edu.co>
2023-05-27 00:42:31 -0500

    Cambios soldado de terracota 14

--- cliente/src/TerracotaSoldier.js
+++ cliente/src/TerracotaSoldier.js
@@ -5,8 +5,8 @@ export default function TerracotaSoldier(props) {
   const { nodes, materials } = useGLTF("./static/TerracotaSoldier.glb");
   return (
     <group {...props} dispose={null}>
-      <group rotation={[-Math.PI / 12, 0, -Math.PI+ -Math.PI / 5] } scale={2}>
-        <group rotation={[Math.PI / 12, 0, 0]}>
+      <group rotation={[-Math.PI / 2, 0, -Math.PI+ -Math.PI / 5] } scale={2}>
+        <group rotation={[Math.PI / 2, 0, 0]}>
           <mesh
             castShadow
             receiveShadow
@@ -19,6 +19,9 @@ export default function TerracotaSoldier(props) {
             geometry={nodes.defaultMaterial_1.geometry}
             material={materials.None}
           />
+          <mesh position-y={12} rotation-x={-Math.PI * 0.5} scale={resize}>
+            <planeGeometry />
+          </mesh>
         </group>
       </group>
     </group>
