68c5a9206e502f513208a26f566a0b4d53376c3c
Juan Esteban Brand Tovar <jose.becerra@correounivalle.edu.co - brand.juan@correounivalle.edu.co - juan.pablo.pantoja@correounivalle.edu.co>
2023-06-09 22:52:24 -0500

    Cambio del front (Login)

--- cliente/src/Form.jsx
+++ cliente/src/Form.jsx
@@ -2,6 +2,17 @@ import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import Index2 from './index2';
+import {
+  MDBBtn,
+  MDBContainer,
+  MDBRow,
+  MDBCol,
+  MDBIcon,
+  MDBInput
+}
+from 'mdb-react-ui-kit';
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+
 
 function App() {
   return (
@@ -32,25 +43,67 @@ function LoginPage() {
   };
 
   return (
-    <div>
-      <h1>Login Page</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="email"
-          placeholder="Correo electrónico"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="Contraseña"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button type="submit">Iniciar sesión</button>
-      </form>
-      <p>{message}</p>
-    </div>
+    <MDBContainer fluid>
+      <MDBRow>
+        <MDBCol sm='6'>
+          <div className='d-flex flex-row ps-1 pt-1'>
+            <MDBIcon fas icon='crow fa-3x me-3' style={{ color: '#709085' }} />
+            <img
+            src='https://www.logocrea.com/wp-content/uploads/2016/07/buda1.png'
+            alt='Login image'
+            className='w-0.1'
+            style={{ objectPosition: 'left' }}
+          />
+          </div>
+
+          <div className='d-flex flex-column justify-content-center h-custom-2 w-75 pt-4'>
+            <h3 className='fw-normal mb-3 ps-5 pb-3' style={{ letterSpacing: '1px' }}>
+              Log in
+            </h3>
+
+            <form onSubmit={handleSubmit}>
+              <MDBInput
+                wrapperClass='mb-4 mx-5 w-100'
+                label='Email address'
+                id='formControlLg'
+                type='email'
+                size='lg'
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <MDBInput
+                wrapperClass='mb-4 mx-5 w-100'
+                label='Password'
+                id='formControlLg'
+                type='password'
+                size='lg'
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+
+              <MDBBtn className='mb-4 px-5 mx-5 w-100' color='info' size='lg' type='submit'>
+                Login
+              </MDBBtn>
+            </form>
+
+            <p className='small mb-5 pb-lg-3 ms-5'>
+              <a className='text-muted' href='#!'>
+                Forgot password?
+              </a>
+            </p>
+          </div>
+        </MDBCol>
+
+        <MDBCol sm='6' className='d-none d-sm-block px-0'>
+          <img
+            src='https://upload.wikimedia.org/wikipedia/commons/7/7c/Soldier_Horse.JPG'
+            alt='Login image'
+            className='w-100'
+            style={{ objectFit: 'cover', objectPosition: 'left' }}
+          />
+        </MDBCol>
+      </MDBRow>
+    </MDBContainer>
   );
 }
 
