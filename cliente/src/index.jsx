a18c32e24ed595507b3f9de7ea0819536c0f016a
Juan Esteban Brand Tovar <jose.becerra@correounivalle.edu.co - brand.juan@correounivalle.edu.co - juan.pablo.pantoja@correounivalle.edu.co>
2023-06-16 21:36:13 -0500

    Pruebas de nueva pagina principal

--- cliente/src/index.jsx
+++ cliente/src/index.jsx
@@ -2,13 +2,13 @@ import './style.css'
 import ReactDOM from 'react-dom/client'
 import { StrictMode } from 'react'
 import Form from './Form'
-import HTMLViewer from './aja'
+import Prueba from './prueba'
 
 const root = ReactDOM.createRoot(document.querySelector('#root'))
 
 root.render(
     <StrictMode>
-        <HTMLViewer/>
+        <Prueba/>
     </StrictMode>
 )
 
