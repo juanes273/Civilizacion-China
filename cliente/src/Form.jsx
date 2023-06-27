f72b2357a466dda15f5340a1bf5cda4bf9040f1c
Juan Esteban Brand Tovar <jose.becerra@correounivalle.edu.co - brand.juan@correounivalle.edu.co - juan.pablo.pantoja@correounivalle.edu.co>
2023-06-27 14:34:10 -0500

    Intento de commit

--- cliente/src/Form.jsx
+++ cliente/src/Form.jsx
@@ -1,112 +1,150 @@
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Routes, useNavigate } from 'react-router-dom';
-import axios from 'axios';
-import Index2 from './index2';
-import Principal from './PaginaP';
+  import React, { useState, useEffect } from 'react';
+  import { BrowserRouter as Router, Route, Routes, useNavigate } from 'react-router-dom';
+  import axios from 'axios';
+  import Index2 from './index2';
+  import Principal from './PaginaP.jsx';
 
-import {
-  MDBBtn,
-  MDBContainer,
-  MDBRow,
-  MDBCol,
-  MDBIcon,
-  MDBInput
-}
-from 'mdb-react-ui-kit';
-import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+  import {
+    MDBBtn,
+    MDBContainer,
+    MDBRow,
+    MDBCol,
+    MDBIcon,
+    MDBInput
+  }
+  from 'mdb-react-ui-kit';
+  import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 
+  function App() {
+    return (
+      <Router>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/Home" element={<Principal />} />
+        </Routes>
+      </Router>
+    );
+  }
 
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/Home" element={<Principal />} />
-      </Routes>
-    </Router>
-  );
-}
+  function LoginPage() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [message, setMessage] = useState('');
+    const [isLoading, setIsLoading] = useState(false); // Nuevo estado para controlar la ventana de carga
+    const [loadingText, setLoadingText] = useState('Cargando'); // Nuevo estado para el texto de carga
+    const navigate = useNavigate();
 
-function LoginPage() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [message, setMessage] = useState('');
-  const navigate = useNavigate();
+    useEffect(() => {
+      // Cambiar el texto de carga cada segundo
+      const interval = setInterval(() => {
+        setLoadingText((prevText) => prevText === 'Cargando...' ? 'Cargando' : prevText + '.');
+      }, 1000);
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const response = await axios.post('https://civilizacion-china.onrender.com/api/login', { email, password });
-      setMessage(response.data.message);
-      navigate('/home');
-    } catch (error) {
-      console.log(error);
-    }
-  };
+      return () => clearInterval(interval);
+    }, []);
 
-  return (
-    <MDBContainer fluid>
-      <MDBRow>
-        <MDBCol sm='6'>
-          <div className='d-flex flex-row ps-1 pt-1'>
-            <MDBIcon fas icon='crow fa-3x me-3' style={{ color: '#709085' }} />
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+      try {
+        setIsLoading(true); // Mostrar la ventana de carga
+        const response = await axios.post('https://civilizacion-china.onrender.com/api/login', { email, password });
+        setMessage(response.data.message);
+        navigate('/Home');
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    return (
+      <MDBContainer fluid>
+        <MDBRow>
+          <MDBCol sm='6'>
+            <div className='d-flex flex-row ps-1 pt-1'>
+              <MDBIcon fas icon='crow fa-3x me-3' style={{ color: '#709085' }} />
+              <img
+                src='https://www.logocrea.com/wp-content/uploads/2016/07/buda1.png'
+                alt='Login image'
+                className='w-0.1'
+                style={{ objectPosition: 'left' }}
+              />
+            </div>
+
+            <div className='d-flex flex-column justify-content-center h-custom-2 w-75 pt-4'>
+              <h3 className='fw-normal mb-3 ps-5 pb-3' style={{ letterSpacing: '1px' }}>
+                Log in
+              </h3>
+
+              <form onSubmit={handleSubmit}>
+                <MDBInput
+                  wrapperClass='mb-4 mx-5 w-100'
+                  label='Email address'
+                  id='formControlLg'
+                  type='email'
+                  size='lg'
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <MDBInput
+                  wrapperClass='mb-4 mx-5 w-100'
+                  label='Password'
+                  id='formControlLg'
+                  type='password'
+                  size='lg'
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+
+                <MDBBtn className='mb-4 px-5 mx-5 w-100' color='info' size='lg' type='submit'>
+                  Login
+                </MDBBtn>
+              </form>
+
+              <p className='small mb-5 pb-lg-3 ms-5'>
+                <a className='text-muted' href='#!'>
+                  Forgot password?
+                </a>
+              </p>
+            </div>
+          </MDBCol>
+
+          <MDBCol sm='6' className='d-none d-sm-block px-0'>
             <img
-            src='https://www.logocrea.com/wp-content/uploads/2016/07/buda1.png'
-            alt='Login image'
-            className='w-0.1'
-            style={{ objectPosition: 'left' }}
-          />
+              src='https://upload.wikimedia.org/wikipedia/commons/7/7c/Soldier_Horse.JPG'
+              alt='Login image'
+              className='w-100'
+              style={{ objectFit: 'cover', objectPosition: 'left' }}
+            />
+          </MDBCol>
+        </MDBRow>
+
+        {/* Código para la ventana de carga */}
+        {isLoading && (
+          <div
+            style={{
+              position: 'fixed',
+              top: '0',
+              left: '0',
+              width: '100%',
+              height: '100%',
+              backgroundColor: 'rgba(0, 0, 0, 0.7)',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              zIndex: '9999'
+            }}
+          >
+            <div
+              className='spinner-border text-light'
+              role='status'
+              style={{ width: '3rem', height: '3rem' }}
+            >
+              <span className='visually-hidden'>Loading...</span>
+            </div>
+            <p className="text-light ms-2">{loadingText}</p> {/* Mostrar el texto de carga */}
           </div>
+        )}
+      </MDBContainer>
+    );
+  }
 
-          <div className='d-flex flex-column justify-content-center h-custom-2 w-75 pt-4'>
-            <h3 className='fw-normal mb-3 ps-5 pb-3' style={{ letterSpacing: '1px' }}>
-              Log in
-            </h3>
-
-            <form onSubmit={handleSubmit}>
-              <MDBInput
-                wrapperClass='mb-4 mx-5 w-100'
-                label='Email address'
-                id='formControlLg'
-                type='email'
-                size='lg'
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-              <MDBInput
-                wrapperClass='mb-4 mx-5 w-100'
-                label='Password'
-                id='formControlLg'
-                type='password'
-                size='lg'
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-
-              <MDBBtn className='mb-4 px-5 mx-5 w-100' color='info' size='lg' type='submit'>
-                Login
-              </MDBBtn>
-            </form>
-
-            <p className='small mb-5 pb-lg-3 ms-5'>
-              <a className='text-muted' href='#!'>
-                Forgot password?
-              </a>
-            </p>
-          </div>
-        </MDBCol>
-
-        <MDBCol sm='6' className='d-none d-sm-block px-0'>
-          <img
-            src='https://upload.wikimedia.org/wikipedia/commons/7/7c/Soldier_Horse.JPG'
-            alt='Login image'
-            className='w-100'
-            style={{ objectFit: 'cover', objectPosition: 'left' }}
-          />
-        </MDBCol>
-      </MDBRow>
-    </MDBContainer>
-  );
-}
-
-export default App;
+  export default App;
--- cliente/src/PaginaP.jsx
+++ cliente/src/PaginaP.jsx
@@ -10,8 +10,22 @@ function Principal() {
   return (
     <Wrapper>
       <Background src={backgroundImage} alt="background" />
-      <Title>Civilizacion antigua china</Title>
-      <Description>Descubre de manera inmersiva esta cultura</Description>
+      <Title>La gran Muralla china</Title>
+      <Description>Descubre de manera inmersiva esta maravilla</Description>
+      <Container>
+        <ModelViewer>
+          {/* Aquí puedes insertar tu código para cargar y mostrar el modelo .glb */}
+        </ModelViewer>
+        <Paragraph>
+          La Gran Muralla China es una impresionante obra arquitectónica que se extiende a lo largo de miles de kilómetros en el norte de China. Construida durante varias dinastías chinas a lo largo de varios siglos, esta majestuosa estructura defensiva es un testimonio del ingenio y la habilidad humana.
+
+La Muralla China fue diseñada originalmente para proteger el territorio chino de las invasiones y ataques de las tribus nómadas del norte. Con una altura que varía entre los 5 y 8 metros y una anchura suficiente para permitir que varios soldados marchen en formación, la muralla estaba equipada con torres de vigilancia estratégicamente ubicadas que permitían una amplia visibilidad y comunicación a lo largo de su extensión.
+
+Esta maravilla arquitectónica no solo cumplía una función militar, sino que también servía como un símbolo de la grandeza y la determinación del antiguo imperio chino. Además de su propósito defensivo, la Muralla China también facilitaba el control del comercio y las comunicaciones a lo largo de la antigua Ruta de la Seda.
+
+Hoy en día, la Gran Muralla China sigue siendo uno de los destinos turísticos más famosos del mundo, atrayendo a millones de visitantes cada año. Su belleza impresionante, su historia cautivadora y su importancia cultural la convierten en un lugar emblemático que refleja la grandeza y el legado de la antigua civilización china.
+        </Paragraph>
+      </Container>
       <StyledButton to="/otrocomponente">Ir a tour 3D</StyledButton>
     </Wrapper>
   );
@@ -51,6 +65,26 @@ const Background = styled.img`
   width: 100%;
   top: 0px;
   z-index: -1;
+`;
+
+const Container = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 20px;
+  align-items: center;
+  margin-top: 40px;
+`;
+
+const ModelViewer = styled.div`
+  /* Estilos para el contenedor del modelo .glb */
+`;
+
+const Paragraph = styled.p`
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  font-size: 16px;
+  line-height: 24px;
+  color: #ffffff;
 `;
 
 const StyledButton = styled(Link)`
